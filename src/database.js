c6d858b70b54bb2520ded270655dab691ff187b9
miguelangel1200 <75793818+miguelangel1200@users.noreply.github.com>
2022-11-10 13:12:44 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Al utilizar Mongo, de manera automatica se crea la base de datos prueba-db-app
-mongoose.connect('mongodb+srv://miguelangel2000:1v5PS3dSVOJ33ik4@cluster0.couuwac.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.couuwac.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
     .then(db => console.log('DB is connect'))
